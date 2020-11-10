0c5201270e04a4438001bd5d71810fbf8e886eb2
bh1huy <65691255+bh1huy@users.noreply.github.com>
2020-11-10 09:05:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,9 @@
 #
 
 # Modify default IP
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
 sed -i 's/192.168.1.1/192.168.136.5/g' package/base-files/files/bin/config_generate
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /BH1HUY build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
